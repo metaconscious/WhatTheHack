b80e408da93c52bf8b6177abe3806fa9e05c24de
metaconscious <94597129+metaconscious@users.noreply.github.com>
2022-10-05 20:07:07 +0800

    reimplement by static auto padding macro credit by @Public_Void from GH, tutorial 20 completed.

---
+++ game/include/autopad.h
@@ -0,0 +1,13 @@
+//
+// Created by user on 10/5/2022.
+//
+
+#ifndef WHATTHEHACK_GAME_INCLUDE_AUTOPAD_H_
+#define WHATTHEHACK_GAME_INCLUDE_AUTOPAD_H_
+
+#define STR_MERGE_IMPL(a, b) a##b
+#define STR_MERGE(a, b) STR_MERGE_IMPL(a, b)
+#define MAKE_PAD(size) STR_MERGE(_pad, __COUNTER__)[size]
+#define DEFINE_MEMBER_N(type, name, offset) struct {unsigned char MAKE_PAD(offset); type name;}
+
+#endif //WHATTHEHACK_GAME_INCLUDE_AUTOPAD_H_
--- game/include/entity.h
+++ game/include/entity.h
@@ -6,48 +6,35 @@
 #define WHATTHEHACK_GAME_INCLUDE_ENTITY_H_
 
 #include <cstdint>
+#include "autopad.h"
 #include "base.h"
+
+class AmmoClip;
+class Weapon;
 
 class PlayerEntity
 {
 public:
-    Vector3 headPosition; //0x0004
-    char pad_0010[36]; //0x0010
-    Vector3 bodyPosition; //0x0034
-    Vector3 viewingAngle; //0x0040
-    char pad_004C[172]; //0x004C
-    int32_t health; //0x00F8
-    char pad_00FC[297]; //0x00FC
-    char name[16]; //0x0225
-    char pad_0235[319]; //0x0235
-    class Weapon *currentWeapon; //0x0374
-
-    virtual void Function0();
-    virtual void Function1();
-    virtual void Function2();
-    virtual void Function3();
-    virtual void Function4();
-    virtual void Function5();
-    virtual void Function6();
-    virtual void Function7();
-    virtual void Function8();
-    virtual void Function9();
-}; //Size: 0x0378
-static_assert(sizeof(PlayerEntity) == 0x378);
+    union
+    {
+        DEFINE_MEMBER_N(int32_t, health, 0xf8);
+        DEFINE_MEMBER_N(Weapon*, currentWeapon, 0x374);
+    };
+};
 
 class Weapon
 {
 public:
-    char pad_0000[20]; //0x0000
-    class AmmoClip *ammoClip; //0x0014
-}; //Size: 0x0018
-static_assert(sizeof(Weapon) == 0x18);
+    union
+    {
+        DEFINE_MEMBER_N(AmmoClip*, ammoClip, 0x14);
+    };
+};
 
 class AmmoClip
 {
 public:
-    int32_t ammo; //0x0000
-}; //Size: 0x0004
-static_assert(sizeof(AmmoClip) == 0x4);
+    int32_t ammo;
+};
 
 #endif //WHATTHEHACK_GAME_INCLUDE_ENTITY_H_
